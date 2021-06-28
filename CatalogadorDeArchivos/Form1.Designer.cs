95320fa1c35cae702f3d654b669204697c7b9a3d
Victor Gabardini <vicgaba@gmail.com>
2021-06-28 18:20:02 -0300

    Primer commit

--- CatalogadorDeArchivos/Form1.Designer.cs
+++ CatalogadorDeArchivos/Form1.Designer.cs
@@ -30,6 +30,17 @@ namespace CatalogadorDeArchivos
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.textBoxArchivos = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.explorarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -37,20 +48,83 @@ namespace CatalogadorDeArchivos
             this.panel1.AutoScroll = true;
             this.panel1.AutoSize = true;
             this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel1.Controls.Add(this.splitContainer1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1486, 751);
+            this.panel1.Size = new System.Drawing.Size(1464, 784);
             this.panel1.TabIndex = 0;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.textBoxArchivos);
+            this.splitContainer1.Panel1.Controls.Add(this.menuStrip1);
+            this.splitContainer1.Panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel1_Paint);
+            this.splitContainer1.Size = new System.Drawing.Size(1464, 784);
+            this.splitContainer1.SplitterDistance = 488;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.SelectedPath = "C:\\Users\\victor.gabardini\\Desktop";
+            this.folderBrowserDialog1.HelpRequest += new System.EventHandler(this.folderBrowserDialog1_HelpRequest);
+            // 
+            // textBoxArchivos
+            // 
+            this.textBoxArchivos.Location = new System.Drawing.Point(5, 246);
+            this.textBoxArchivos.Multiline = true;
+            this.textBoxArchivos.Name = "textBoxArchivos";
+            this.textBoxArchivos.Size = new System.Drawing.Size(473, 340);
+            this.textBoxArchivos.TabIndex = 1;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(488, 33);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.explorarToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(187, 29);
+            this.toolStripMenuItem1.Text = "toolStripMenuItem1";
+            // 
+            // explorarToolStripMenuItem
+            // 
+            this.explorarToolStripMenuItem.Name = "explorarToolStripMenuItem";
+            this.explorarToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.explorarToolStripMenuItem.Text = "Explorar";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1486, 751);
+            this.ClientSize = new System.Drawing.Size(1464, 784);
             this.Controls.Add(this.panel1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -59,6 +133,12 @@ namespace CatalogadorDeArchivos
         #endregion
 
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox textBoxArchivos;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem explorarToolStripMenuItem;
     }
 }
 
--- CatalogadorDeArchivos/Form1.cs
+++ CatalogadorDeArchivos/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,53 @@ namespace CatalogadorDeArchivos
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+                
+        }
+
+
+        public static string GetFiles(string path)
+        {
+            string str = "";
+            DirectoryInfo dir = new DirectoryInfo(path);
+            foreach (FileSystemInfo f in dir.GetFileSystemInfos())
+            {
+                if (f is FileInfo)
+                {
+
+                    FileInfo j = f as FileInfo;
+                    str += "" + f.Name + " - " + j.Length + "\r\n";
+                }
+                else
+                {
+                    str += "\\" + f.Name + "\r\n";
+                }
+            }
+            return str;
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxExplorador_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxExplorador_Load(Object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.ShowDialog();
+            string path = fbd.SelectedPath;
+            if (path != "")
+            {
+                textBoxArchivos.Text = GetFiles(path);
+            }
         }
     }
 }
